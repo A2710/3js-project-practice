0e402fbb9041a5a75183b6ab51319c673aabc499
A2710 <adityaagarwal2710@gmail.com>
2024-08-30 21:29:41 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
 import './App.css'
 import Navbar from './components/navbar'
-
+import Home from './pages/home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +14,10 @@ function App() {
     <Router>
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={'Home'}></Route>
-        <Route path="/about" element={'About'}></Route>
-        <Route path="/projects" element={'Projects'}></Route>
-        <Route path="/contact" element={'Contact'}></Route>                
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/projects" element={<Projects/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>                
       </Routes>
     </Router>
   </main>
