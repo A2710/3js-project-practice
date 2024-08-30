f4aa5ad0ab215973fc04ea7fff3151b1490287ef
A2710 <adityaagarwal2710@gmail.com>
2024-08-30 21:23:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import './App.css'
+import Navbar from './components/navbar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <main className='bg-slate-300/20'>
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={'Home'}></Route>
+        <Route path="/about" element={'About'}></Route>
+        <Route path="/projects" element={'Projects'}></Route>
+        <Route path="/contact" element={'Contact'}></Route>                
+      </Routes>
+    </Router>
+  </main>
+}
+export default App
